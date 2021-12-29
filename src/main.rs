2b63a3d87a5442165be56af1141747cf1d1f6151
thesecretmaster <thesecretmaster@developingtechnician.com>
2021-12-29 11:47:12 -0600

    About to start redesign to go from a queue trait and a lockfreequeue trait to a newtype system where we'll enforce SRSW

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,40 @@ trait Queue<D> {
     fn len(&self) -> usize;
 }
 
-trait ImmutableQueue<D> {
-    fn new(capacity: usize) -> Self;
-    fn push(&self, _: D) -> bool;
-    fn pop(&self) -> Option<D>;
-    fn closed(&self) -> bool;
-    fn close(&self);
+unsafe trait LockFreeQueue<D>: Queue<D> {
+    fn new<E: Queue<D>>(capacity: usize) -> E {
+        Queue::new(capacity)
+    }
+
+    fn push(&self, data: D) -> bool {
+        let q = self as *const Self;
+        let qm = q as *mut Self;
+        unsafe { Queue::push(&mut *qm, data) }
+    }
+
+
+    fn pop(&mut self) -> Option<D> {
+        let q = self as *const Self;
+        let qm = q as *mut Self;
+        unsafe { Queue::pop(&mut *qm) }
+    }
+
+    fn closed(&self) -> bool {
+        Queue::closed(self)
+    }
+
+    fn close(&mut self);
     fn capacity(&self) -> usize;
     fn len(&self) -> usize;
 }
-
-struct LamportQueue<D: Default + Clone + Sized> {
+struct LamportQueue<D: Default + Clone> {
     elements: Box<[D]>,
     head: usize,
     tail: usize,
     open: bool
 }
 
-impl<D: Default + Clone + Sized> Queue<D> for LamportQueue<D> {
+impl<D: Default + Clone> Queue<D> for LamportQueue<D> {
     fn new(capacity: usize) -> LamportQueue<D> {
         let buffer = vec![D::default(); capacity];
         Self {
@@ -91,30 +107,32 @@ impl<D: Default + Clone + Sized> Queue<D> for LamportQueue<D> {
     }
 }
 
-struct AtomicLamportQueue<D: Clone + Default + Sized> {
+struct AtomicLamportQueue<D: Clone + Default> {
     v: UnsafeCell<LamportQueue<D>>
 }
 
+unsafe impl<T: Clone + Default> Sync for AtomicLamportQueue<T> {}
 
-
-fn mut_thru_arc<D: Sized + Clone + Default>(a: &mut Arc<AtomicLamportQueue<D>>) -> &mut LamportQueue<D> {
+// This feels like it should be an impl DerefMut on Arc<AtomicLamportQueue<D>>, but
+// I can't impliment DerefMut on Arc since it's not my type
+fn mut_thru_arc<D: Clone + Default>(a: &mut Arc<AtomicLamportQueue<D>>) -> &mut LamportQueue<D> {
     unsafe { a.v.get().as_mut().unwrap() }
 }
 
-impl<D: Default + Sized + Clone> Deref for AtomicLamportQueue<D> {
+impl<D: Default + Clone> Deref for AtomicLamportQueue<D> {
     type Target = LamportQueue<D>;
 
     fn deref(&self) -> &Self::Target {
         unsafe { self.v.get().as_ref().unwrap() }
     }
 }
-impl<D: Default + Sized + Clone> DerefMut for AtomicLamportQueue<D> {
+impl<D: Default + Clone> DerefMut for AtomicLamportQueue<D> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { self.v.get().as_mut().unwrap() }
     }
 }
 
-impl<D: Clone + Default + Sized> AtomicLamportQueue<D> {
+impl<D: Clone + Default> AtomicLamportQueue<D> {
     fn new(capacity: usize) -> AtomicLamportQueue<D> {
         Self {
             v: UnsafeCell::new(LamportQueue::new(capacity))
@@ -122,45 +140,60 @@ impl<D: Clone + Default + Sized> AtomicLamportQueue<D> {
     }
 }
 
-unsafe impl<T: Clone + Default + Sized> Sync for AtomicLamportQueue<T> {}
-
-const SIZE: usize = 10000;
+const SIZE: usize = 10000; // Number of random numbers to use to test the queue
 
 fn main() {
-    let q: AtomicLamportQueue<u32> = AtomicLamportQueue::new(3);
-    let shared_q = Arc::new(q);
-    let mut sender_handle = Arc::clone(&shared_q);
-    let mut reciever_handle = Arc::clone(&shared_q);
+    // Create the queue and prepare it for sharing
+    let queue: AtomicLamportQueue<u32> = AtomicLamportQueue::new(3);
+    let shared_queue = Arc::new(queue);
+    let mut sender_handle = Arc::clone(&shared_queue);
+    let mut reciever_handle = Arc::clone(&shared_queue);
 
+    // Generate a list of random numbers to send down the queue to ensure it's
+    // somewhat sane
     let mut rng_list = [0; SIZE];
     let mut recieved_numbers = Vec::with_capacity(SIZE);
     for i in &mut rng_list {
         *i = rand::thread_rng().gen()
     }
+
     atomic::compiler_fence(atomic::Ordering::SeqCst);
+
     let sender = thread::spawn(move || {
         for i in &rng_list {
-            loop {
-                if mut_thru_arc(&mut sender_handle).push(*i) {
-                    break
-                }
-            }
+            // Continually retry until the push is sucessful
+            while !mut_thru_arc(&mut sender_handle).push(*i) { }
         }
+        // Close the loop to allow the reciever to terminate
         mut_thru_arc(&mut sender_handle).close();
+        rng_list
     });
+
     let reciever = thread::spawn(move || {
-        loop {
+        // Continually pop off the queue until it's closed and empty
+        while !(reciever_handle.closed() && reciever_handle.len() == 0) {
             match mut_thru_arc(&mut reciever_handle).pop() {
                 None => (),
                 Some(i) => {
                     recieved_numbers.push(i)
                 }
             }
-            if reciever_handle.closed() && reciever_handle.len() == 0 {
-                break
-            }
         }
+        recieved_numbers
     });
-    sender.join().unwrap();
-    reciever.join().unwrap();
+
+    let rng_list = sender.join().unwrap();
+    let recieved_numbers = reciever.join().unwrap();
+
+    // Correctness checks
+    let same_length = rng_list.len() == recieved_numbers.len();
+    let mut numbers_match = true;
+    for i in 0..SIZE {
+        if rng_list[i] != recieved_numbers[i] {
+            numbers_match = false;
+            break;
+        }
+    }
+    println!("Count is correct? {}", same_length);
+    println!("Numbers match up? {}", numbers_match);
 }
